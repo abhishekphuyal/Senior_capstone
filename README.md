4743e5871ea93540145062ba32541fd290a44d37
abhishekphuyal <82634414+abhishekphuyal@users.noreply.github.com>
2021-07-07 22:15:56 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,3 @@
+# Junior_capstone
+
+This code is for a project for my junior year capstone project that projects what I have learned thus far at c-tec, the code here is not copyrighted material and available to everyone, however I will ask that there be credit given where credit is due
---
+++ Script.js
@@ -0,0 +1,17 @@
+var James = new Audio();
+James.src = 'James.mp3';
+
+var Iank = new Audio();
+Iank.src = 'Iank.mp3';
+
+var Joshw = new Audio();
+Joshw.src = 'Joshw.mp3';
+
+var JoshR = new Audio();
+JoshR.src = 'JoshR.mp3';
+
+var Danb = new Audio();
+Danb.src = 'Danb.mp3';
+
+var Welkie = new Audio();
+Welkie.src = "Welkie.mp3";
